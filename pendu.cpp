b954ff3f73818dd29329faa0b96d8aca4d2b2db0
pgalmiche <pgalmiche9@gmail.com>
2019-02-18 11:00:23 +0100

    Premier get_letter + includes

--- pendu.cpp
+++ pendu.cpp
@@ -1,7 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <ctype.h>
+
 /**
  * Jeu du pendu pour terminal
  *
- * Authors : Léo Bois, ...
+ * Authors : Léo Bois, Pierre Galmiche, ...
  *
  */
 
@@ -20,8 +26,6 @@ class Game
 	private:
 		std::string		random_word();
 
-
-
 		std::vector<char>	sword;// Par exemple SECRET (S E C R E T)
 		std::vector<bool>	found;// Par exemple FFTFFT (_ _ C _ _ T)
 
@@ -29,10 +33,23 @@ class Game
 		bool			is_over;
 };
 
-char get_letter()
+char get_letter()//Pierre
 {
+    char letter;
+    bool correct = false;
 
-}
+    while (!correct)
+    {
+        std::cout << " > ";
+        std::cin >> letter;
+
+        if (!(isalpha(letter)))
+            std::cout << "Not a letter, try again." << std::endl;
+        else
+            correct = true;
+    }
+    return toupper(letter);
+}//Retourne la lettre entrée en majuscule
 
 int main()
 {
@@ -44,6 +61,7 @@ int main()
 		game.print_state();
 		
 		// Demande au joueur de saisir une lettre (et une seule)
+		char a;
 		a = get_letter();
 		// Met à jour les attributs de game
 		game.submit(a);
